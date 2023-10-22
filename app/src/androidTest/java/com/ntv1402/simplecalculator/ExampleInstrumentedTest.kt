28193152e9f9ad512fadaa36bd15821d66d28cb7
ntv1402 <103202487+ntv1402@users.noreply.github.com>
2023-10-22 23:15:43 +0700

    Update ExampleInstrumentedTest.kt

--- app/src/androidTest/java/com/ntv1402/simplecalculator/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/ntv1402/simplecalculator/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.bn2002.simplecalculator
+package com.ntv1402.simplecalculator
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.bn2002.simplecalculator", appContext.packageName)
+        assertEquals("com.ntv1402.simplecalculator", appContext.packageName)
     }
 }
