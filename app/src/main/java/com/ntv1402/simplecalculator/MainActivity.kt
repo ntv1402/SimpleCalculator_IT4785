e186b829f0c214d7fb34914aa8381eed59f0c041
ntv1402 <103202487+ntv1402@users.noreply.github.com>
2023-10-22 23:16:27 +0700

    Update MainActivity.kt

--- app/src/main/java/com/ntv1402/simplecalculator/MainActivity.kt
+++ app/src/main/java/com/ntv1402/simplecalculator/MainActivity.kt
@@ -1,9 +1,9 @@
-package com.bn2002.simplecalculator
+package com.ntv1402.simplecalculator
 
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import com.bn2002.simplecalculator.databinding.ActivityMainBinding
+import com.ntv1402.simplecalculator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
